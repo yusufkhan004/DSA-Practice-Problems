53d148c86e47104608d67602df250e5b725a6b1a
yusufkhan004 <khanyusuf004@gmail.com>
2021-08-09 20:51:15 +0530

    classes and objects using an example

--- Linkedlist/Classes_objects.cpp
+++ Linkedlist/Classes_objects.cpp
@@ -1,5 +1,5 @@
-#include<iostream.h>
-#include<string.h>
+#include <iostream>
+#include <string>
 using namespace std;
 
 // user defined datatype
@@ -24,12 +24,12 @@ class Cars{
         }
 
         void display() {
-            cout << "car properties" << endl;
-            cout << "Company name" << company_name << endl;
-            cout << "Model name" << model_name << endl;
-            cout << "Fuel type" << fuel_type << endl;
-            cout << "mileage" << mileage << endl;
-            cout << "Price" << price << endl;
+            cout << "car properties.:" << endl;
+            cout << "Company name -> " << company_name << endl;
+            cout << "Model name -> " << model_name << endl;
+            cout << "Fuel type -> " << fuel_type << endl;
+            cout << "mileage -> " << mileage << endl;
+            cout << "Price -> " << price << endl;
         }
 };
 
@@ -38,5 +38,6 @@ int main() {
     Cars car1; //objects(car1) are nothing but variables of type class(Cars)
 
     car1.setData("Toyota","altis","petrol",15.5,150000);
+    car1.display();
     return 0;
 }
