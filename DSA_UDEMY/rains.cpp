78f3581992aee7808ae1d3ca8bd0a4bc7d505cb0
yusufkhan004 <khanyusuf004@gmail.com>
2021-08-27 22:19:33 +0530

    error solved

--- DSA_UDEMY/rains.cpp
+++ DSA_UDEMY/rains.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 #include <vector>
-#include<algorithm>
+#include <algorithm>
 
 using namespace std;
 
-int trappedwater(vector <int> heights){
+int trappedwater(vector<int> heights){
 
     //exceptional case when n(width) is less than 2 then return 0
 
@@ -15,17 +15,19 @@ int trappedwater(vector <int> heights){
     }
 
     // for leftMax and rightMax
-    vector <int> left(n,0), right(n,0);
+    vector<int> left(n,0), right(n,0);
     left[0] = heights[0];
     right[n-1] = heights[n-1];
 
     for(int i=1;i<n;i++){
         left[i] = max(left[i-1],heights[i]);
-        right[n-i-1] = max(right[n-1],heights[n-i-1]);
+        right[n-i-1] = max(right[n-i],heights[n-i-1]);
+        cout << left[i] << " " << right[i] <<endl;
     }
 
     for(int i=0;i<n;i++){
-        level = level + (min(left[i],right[i])-heights[i]);
+        level +=min(left[i],right[i])-heights[i];
+        // cout << level << endl;
     }
 
 return level;
@@ -35,6 +37,7 @@ return level;
 int main(){
 
     vector<int> water = {0,1,0,2,1,0,1,3,2,1,2,1};
+    cout << "result" << endl;
     cout << trappedwater(water);
     return 0;
 }
