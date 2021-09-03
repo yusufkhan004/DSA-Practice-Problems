0c192efcf3833394af831c9db811321342da768a
yusufkhan004 <khanyusuf004@gmail.com>
2021-09-03 23:22:00 +0530

    default and parameterized constructor

---
+++ OOPS in C++/defaultconstructor.cpp
@@ -0,0 +1,42 @@
+// Constructors are special class members which are called
+// by the compiler every time an object of that class is
+// instantiated. Constructors have the same name as the
+// class and may be defined inside or outside the class
+// definition.
+
+#include <iostream>
+
+using namespace std;
+
+class Geek
+{
+
+public:
+    int id;
+
+    //default constructor
+    Geek()
+    {
+        cout << "Default constructor is called " << endl;
+        id = -1;
+    }
+
+    //Parameterized constructor
+    Geek(int id)
+    {
+        cout << "Parameterized constructor is called" << endl;
+    }
+};
+
+int main()
+{
+    //obj1 will call default constructor
+    Geek obj1;
+    cout << "ID: : " << obj1.id << endl;
+
+    //obj2 will call parametrized constructor
+    Geek obj2(21);
+    cout << "ID : " << obj2.id << endl;
+
+    return 0;
+}
