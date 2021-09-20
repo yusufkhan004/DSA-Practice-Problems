57e390eb119a8e0cc68b2a19b21c2dbf0fe011f5
yusufkhan004 <khanyusuf004@gmail.com>
2021-09-20 23:00:56 +0530

    corrected factusingmemoization

--- Dynammic_programming/factusingmemoization.cpp
+++ Dynammic_programming/factusingmemoization.cpp
@@ -2,62 +2,36 @@
 #include <map>
 using namespace std;
 
-// int fct(int n, map<int, int> memo)
-// {
-//     int f;
-//     if (memo.find(n) != memo.end())
-//     {
-//         return memo[n];
-//     }
-//     if (n == 0)
-//     {
-//         return 1;
-//     }
-//     if (f <= n)
-//     {
-//         memo[f] = f * fct(f + 1, memo);
-//     }
-//     // memo[n] = n * fct(n - 1, memo);
+int fct(int n, map<int, int> memo)
+{
+    int f;
+    if (memo.find(n) != memo.end())
+    {
+        return memo[n];
+    }
+    if (n == 0)
+    {
+        return 1;
+    }
 
-//     return memo[n];
-// }
+    memo[n] = n * fct(n - 1, memo);
 
-// int fact(int n)
-// {
-//     map<int, int> s;
-//     int f = fct(n, s);
+    return memo[n];
+}
 
-//     return f;
-// }
+int fact(int n)
+{
+    map<int, int> s;
+    int f = fct(n, s);
+
+    return f;
+}
 
 int main()
 {
     int n;
     cin >> n;
-    long long fact = 1;
-    int i = n;
-
-    while (i > 0)
-    {
-        fact = fact * i;
-        i = i - 1;
-    }
-
-    cout << fact;
-    // vector<int> res;
-
-    // while (fact > 0)
-    // {
-    //     res.push_back(fact % 10);
-    //     fact /= 10;
-    // }
-
-    // for (auto i : res)
-    // {
-    //     cout << i << " ";
-    // }
-    // cout << fact;
-    // n = fact(n);
-    // cout << n;
+    n = fact(n);
+    cout << n;
     return 0;
 }
